e9b721776273c981d55640f37ca52f655680e595
Andy <witan03@ca.com>
2017-03-18 20:26:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/jxie/fixturefollower20/MapsActivity.java
@@ -0,0 +1,66 @@
+package com.example.jxie.fixturefollower20;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private Spinner mSpinner;
+
+    private String selection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mSpinner = (Spinner)findViewById(R.id.spinner);
+        mSpinner.setEnabled(true);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.matchdays, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
+    }
+
+
+    public void onClick(View v){
+        selection = mSpinner.getSelectedItem().toString();
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng london = new LatLng(51.4816, -0.191034);
+        mMap.addMarker(new MarkerOptions().position(london).title("Marker in Chelsea"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(london));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(london,6));
+    }
+}
